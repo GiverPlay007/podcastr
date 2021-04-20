316a3155454adc898aff14fea7e03b2677934eef
GiverPlay007 <play.giver.007@gmail.com>
2021-04-20 20:50:52 -0300

    Criando e consumindo fake api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
-export default function Home() {
-  return (
-    <>
-    </>
-  );
+export default function Home(props) {
+  console.log(props.episodes);
+
+  return <>
+  {
+    JSON.stringify(props.episodes)
+  }
+  </>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3001/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 36000
+  };
 }
