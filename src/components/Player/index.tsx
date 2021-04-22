381cd5f403099ba81f453e06624364a85711d1ce
GiverPlay007 <play.giver.007@gmail.com>
2021-04-22 15:00:48 -0300

    Tag de áudio

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -54,6 +54,13 @@ export function Player() {
           </div>
           <span>00:00</span>
         </div>
+        
+          { episode && (
+            <audio
+              src={episode.url}
+              autoPlay 
+            />
+          ) }
 
         <div className={styles.buttons}>
           <button type="button" disabled={!episode}>
