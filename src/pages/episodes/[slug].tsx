14fd90be325e5a1d37ab445a477232f182bac9fe
GiverPlay007 <play.giver.007@gmail.com>
2021-04-21 23:10:19 -0300

    Slugs, breve roteamento.

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,88 @@
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { api } from "../../services/api";
+import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
+
+import Link from "next/link";
+import styles from "./episode.module.scss";
+import Image from "next/image";
+
+type Episode = {
+  duration: number;
+  durationAsString: string;
+  publishedAt: string;
+  description: string;
+  thumbnail: string;
+  members: string;
+  title: string;
+  url: string;
+  id: string;
+}
+
+type EpisodeProps = {
+  episode: Episode;
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+  return (
+    <div className={ styles.episode }>
+      <div className={ styles.thumbnailContainer }>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar"/>
+          </button>
+        </Link>
+        <Image
+          width={700}
+          height={160}
+          src={ episode.thumbnail }
+          objectFit="cover"
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio"/>
+        </button>
+      </div>
+
+      <header>
+        <h1>{ episode.title }</h1>
+        <span>{ episode.members }</span>
+        <span>{ episode.publishedAt }</span>
+        <span>{ episode.durationAsString }</span>
+      </header>
+
+      <div className={ styles.description } dangerouslySetInnerHTML={{ __html: episode.description }} />
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking"
+  };
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { slug } = context.params;
+  const { data } = await api.get(`/episodes/${slug}`);
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    description: data.description,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), "d MMM yy", { locale: ptBR }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    url: data.file.url,
+  };
+
+  return {
+    props: {
+      episode
+    },
+    revalidate: 86400
+  };
+}
